e027f385226c2b87fb41d40d647e85db195dd224
Jeff Escalante <hello@jenius.me>
2015-02-05 17:16:00 -0500

    fix 6to5 tests again

--- test/fixtures/6to5/expected/basic.js
+++ test/fixtures/6to5/expected/basic.js
@@ -4,8 +4,12 @@ var _prototypeProperties = function (child, staticProps, instanceProps) { if (st
 
 var _inherits = function (subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) subClass.__proto__ = superClass; };
 
+var _classCallCheck = function (instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } };
+
 var Test = (function (TestClass) {
   function Test(greeting) {
+    _classCallCheck(this, Test);
+
     this.greeting = greeting;
   }
 
